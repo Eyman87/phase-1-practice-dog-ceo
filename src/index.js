b3b2189f9918c5763d28503fef1a2e8a14c76e99
Eyman Eghterafi <eghterafieyman@gmail.com>
2022-03-19 13:16:09 -0500

    commit tree f8cc83605402e53ad8578132fc6cb82b9653e4cd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+function ceo() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => renderImg(json))
+
+}
+
+function renderImg(images) {
+    const loc = document.querySelector('div')
+    images.message.forEach(image => {
+        const img = document.createElement('img');
+        img.src = image;
+       loc.appendChild(img)
+    });
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ceo()
+})
+
