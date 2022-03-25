688853060573b76475628efe50531c48b582c0e3
Eyman Eghterafi <eghterafieyman@gmail.com>
2022-03-24 21:19:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+    (loadImages(), addBreeds())
+})
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        console.log("response", results.message)
+        const dogImageContainer = document.getElementById("dog-image-container")
+        results.message.forEach(url => {
+            const img = document.createElement("img")
+            img.src = url
+            dogImageContainer.append(img)
+        })
+    })
+}
+
+
+function addBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        console.log("response", results.message)
+        const dogBreeds = document.getElementById("dog-breeds")
+        results.message.keys(breed => {
+            console.log(results.message)
+            const li = document.createElement("li")
+            li.src = breed.key
+            dogBreeds.append(li)
+        })
+    })
+}
+
